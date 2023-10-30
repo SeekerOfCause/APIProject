ac52f6dbc1915dc4e1d730ed962a77f65b216043
Michael <mmaye617@gmail.com>
2023-10-30 18:32:40 -0400

    Setup basic Post/Get functions

---
+++ src/main/java/com/example/espnapimysql/EspnapimysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.espnapimysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspnapimysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspnapimysqlApplication.class, args);
+	}
+
+}
